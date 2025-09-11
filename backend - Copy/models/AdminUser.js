29f846b76ac98ab4c2f3c595b8222f14352c9999
VARSHINI-V22 <varshini22123@gmail.com>
2025-09-11 12:57:44 +0530

    Initial commit

--- backend - Copy/models/AdminUser.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const adminUserSchema = new mongoose.Schema({
-  username: String,
-  password: String, // hashed
-});
-
-export default mongoose.model("AdminUser", adminUserSchema);
--- backend - Copy/models/Category.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const categorySchema = new mongoose.Schema({
-  name: String,
-  image: String,
-});
-
-module.exports = mongoose.model("Category", categorySchema);
