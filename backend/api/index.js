a972bea1dd4ae5d40e81ec67f3e03810eac8f3de
VARSHINI-V22 <varshini22123@gmail.com>
2025-09-12 21:35:35 +0530

    Updated feature/fixed bug/added changes

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,5 +1,5 @@
 const serverless = require("serverless-http");
-const app = require("../server"); // path to your server.js
+const app = require("../server"); // path to server.js
 
 module.exports = app;
 module.exports.handler = serverless(app);
